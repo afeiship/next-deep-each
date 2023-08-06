524d7f55c05960f8bef088b7ede2d02a35b27d55
afeiship <1290657123@qq.com>
2023-08-06 17:03:08 +0800

    fix: typing err

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 interface NxStatic {
   deepEach: (
     target: any,
-    callback: (key: string, value: any, target: any) => void,
+    callback: (key: string, value: any, target: any, isary: boolean, paths: string[]) => void,
     context?: any
   ) => void;
 }
