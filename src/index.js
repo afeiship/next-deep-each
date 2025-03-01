c2058d1defe08330cf07d8b5473f1db6e75ef45f
afeiship <1290657123@qq.com>
2025-03-02 06:37:18 +0800

    fix: rm unsed const

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import nx from '@jswork/next';
 
-const MSG_ERROR_MUST_TARGET = 'Target must be an array or an object.';
+// const MSG_ERROR_MUST_TARGET = 'Target must be an array or an object.';
 
 nx.deepEach = function (inTarget, inCallback, inContext) {
   if (!inTarget || typeof inTarget !== 'object') return;
