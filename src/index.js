4f07b8ceb5f5960bba8b143d4d20e424b9eb477c
afeiship <1290657123@qq.com>
2025-03-01 11:44:34 +0800

    fix: ignore deep each

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import nx from '@jswork/next';
 const MSG_ERROR_MUST_TARGET = 'Target must be an array or an object.';
 
 nx.deepEach = function (inTarget, inCallback, inContext) {
-  if (!inTarget || typeof inTarget !== 'object') nx.error(MSG_ERROR_MUST_TARGET);
+  if (!inTarget || typeof inTarget !== 'object') return;
 
   const each = function (target, paths = []) {
     nx.each(target, function (key, value) {
